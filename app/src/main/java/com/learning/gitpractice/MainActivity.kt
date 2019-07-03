aa4a9d887310d86220a30ca57b4f071754be7b9e
hieunt@kaopiz.com <hieunt@kaopiz.com>
2019-07-03 14:52:44 +0700

    Failure

--- app/src/main/java/com/learning/gitpractice/MainActivity.kt
+++ app/src/main/java/com/learning/gitpractice/MainActivity.kt
@@ -4,15 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btnShowToast.setOnClickListener {
-            Toast.makeText(this,"Git Practice",Toast.LENGTH_SHORT).show()
-        }
+        asdhaksdgahsdoihhfuqwehiqohweriohirohqowhrohq
     }
+
+    private fun IntRange.random(): Int = Random.nextInt((endInclusive + 1) - start) + start
 }
